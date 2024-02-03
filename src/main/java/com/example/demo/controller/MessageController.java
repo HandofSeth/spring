ede750cbbfcb879325db78ca8a4b2356012c99e4
Rem Ibn <91845322+HandofSeth@users.noreply.github.com>
2024-02-03 14:21:09 +0100

    rest

--- src/main/java/com/example/demo/controller/MessageController.java
+++ src/main/java/com/example/demo/controller/MessageController.java
@@ -4,8 +4,9 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MessageController {
     @GetMapping("/msg")
     public String showMessage(Model model)
@@ -15,6 +16,6 @@ public class MessageController {
         model.addAttribute("attr", Name);
 
 
-        return "MessagePage";
+        return Name;
     }
 }
